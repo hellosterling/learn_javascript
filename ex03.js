fd6d8d932533f89a0adeb93763f685e63050746c
Sterling <hellosterling@gmail.com>
2019-02-26 19:47:17 -0800

    Add files via upload

---
+++ ex03.js
@@ -0,0 +1,8 @@
+// You can give multiple things for console.log to print.
+// Separate each thing with a comma.
+console.log("Name:", 'Sterling P. Guelich');
+console.log("Age:", 41);
+console.log("Feet Tall:", Math.round(74 / 12));
+console.log("And Inches:", 74 - (Math.round(74 / 12) * 12));
+console.log("Age * Height:", 41 * 74);
+console.log("Age * Feet:", 43 * Math.round(74 / 12));
